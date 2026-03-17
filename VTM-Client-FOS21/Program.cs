23c7640b7c714b57f771b29f77d24f2e3d18cf4f
bench2828 <benjamin.roscher04@gmail.com>
2026-03-17 21:24:24 +0100

    Program.cs um Attribute und Main() erweitert.

--- VTM-Client-FOS21/Program.cs
+++ VTM-Client-FOS21/Program.cs
@@ -9,14 +9,22 @@ namespace VTM_Client_FOS21
     internal static class Program
     {
         //HIer bitte die Attribute ergänzen
-
+        private IModel model;
+        private IView view;
+        private IController controller;
 
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            IModel model = new IModel();
+            IView view = new IView()
+
+            IController controller = new Controller(model, view);
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
